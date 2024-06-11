8ae11903953eb50f03ef7a9ee6a02aee231f430a
Thano0s <himanshupatil45h@gmail.com>
2024-06-11 09:45:00 +0530

    Note data class created

--- app/src/main/java/dev/than0s/mydiary/screen/diary/Note.kt
+++ app/src/main/java/dev/than0s/mydiary/screen/diary/Note.kt
@@ -1,12 +1,16 @@
 package dev.than0s.mydiary.screen.diary
 
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.google.firebase.firestore.DocumentId
+import com.google.firebase.firestore.ServerTimestamp
 import java.util.Date
 import java.util.UUID
 
 data class Note(
-    val date: Date = Date(),
+    @DocumentId val id:String = "",
+    @ServerTimestamp val date: Date = Date(),
+    val user:String = "",
+    val title:String = "",
     val emoji: ImageVector,
-    val note: String,
-    val uid: String = UUID.randomUUID().toString()
+    val description: String = "",
 )
