5815b1833c0502e3751b7036a4a0fcabcc0d423a
Thano0s <himanshupatil45h@gmail.com>
2024-06-12 09:32:51 +0530

    'my diary view model' implemented

---
+++ app/src/main/java/dev/than0s/mydiary/screen/MyDiaryViewModel.kt
@@ -0,0 +1,23 @@
+package dev.than0s.mydiary.screen
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+open class MyDiaryViewModel : ViewModel() {
+    fun launchCatching(
+        errorMassage: (message: String) -> Unit,
+        block: suspend CoroutineScope.() -> Unit
+    ) =
+        viewModelScope.launch(
+            context = CoroutineExceptionHandler { _, throwable ->
+                throwable.message?.let {
+                    errorMassage(it)
+                }
+            },
+            block = block
+        )
+
+}
---
+++ app/src/main/java/dev/than0s/mydiary/screen/diary/DiaryViewModel.kt
@@ -0,0 +1,4 @@
+package dev.than0s.mydiary.screen.diary
+
+class DiaryViewModel {
+}
