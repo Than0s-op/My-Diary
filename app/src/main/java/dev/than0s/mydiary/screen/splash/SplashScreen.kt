d86475950aa3c6e0c4553667143c93773809c067
Than0s <himanshupatil45h@gmail.com>
2024-07-19 10:25:14 +0530

    fix splash screen

--- app/src/main/java/dev/than0s/mydiary/screen/splash/SplashScreen.kt
+++ app/src/main/java/dev/than0s/mydiary/screen/splash/SplashScreen.kt
@@ -1,6 +1,7 @@
 package dev.than0s.mydiary.screen.splash
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import dev.than0s.mydiary.DIARY_SCREEN
 
@@ -9,4 +10,15 @@ fun SplashScreen(viewModel: SplashViewModel = hiltViewModel(), popAndOpen: (Stri
     viewModel.loadUser {
         popAndOpen(DIARY_SCREEN)
     }
+}
+
+@Composable
+fun SplashScreenContent() {
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun SplashScreenPreview() {
+
 }
