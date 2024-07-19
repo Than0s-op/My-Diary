d9dee88f296884b8b8afcd1d58233207aa1facea
Than0s <himanshupatil45h@gmail.com>
2024-07-19 10:27:52 +0530

    remove 'account service' from diary view model

--- app/src/main/java/dev/than0s/mydiary/screen/diary/DiaryViewModel.kt
+++ app/src/main/java/dev/than0s/mydiary/screen/diary/DiaryViewModel.kt
@@ -10,7 +10,6 @@ import javax.inject.Inject
 
 @HiltViewModel
 class DiaryViewModel @Inject constructor(
-    accountService: AccountServiceImple,
     storageService: StorageService
 ) : MyDiaryViewModel() {
     val notes = storageService.notes
